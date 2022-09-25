879a496c9b5272db136e7213ea24c88421affc79
Gustavo Neves <gustavoeyros@hotmail.com>
2022-09-25 18:41:04 -0300

    insert

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const exphbs = require('express-handlebars')
 const pool = require('./db/conn')
 const app = express()
 
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 app.use(express.static('public')) //css
@@ -11,6 +18,17 @@ app.get('/', (req,res)=>{
     res.render('home')
 })
 
+//Inserindo as tarefas
+app.post('/tasks/insert', (req, res)=>{
+    const task = req.body.task
+    const sql = `INSERT INTO tasks (task) VALUES ('${task}')`;
+    pool.query(sql, function(err){
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
 
 
 
