bff6ab7d8a79184cf8c7e392e83731f992e74afa
Gustavo Neves <gustavoeyros@hotmail.com>
2022-09-26 17:25:46 -0300

    delete

--- index.js
+++ index.js
@@ -15,17 +15,6 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public')) //css
 
 
-//Inserindo as tarefas
-app.post('/tasks/insert', (req, res)=>{
-    const task = req.body.task
-    const sql = `INSERT INTO tasks (task) VALUES ('${task}')`;
-    pool.query(sql, function(err){
-        if(err){
-            console.log(err)
-        }
-        res.redirect('/')
-    })
-})
 
 app.get('/', (req,res)=>{
     //Visualizar as tarefas
@@ -40,6 +29,56 @@ app.get('/', (req,res)=>{
    
 })
 
+
+//Inserindo as tarefas
+app.post('/tasks/insert', (req, res)=>{
+    const task = req.body.task
+    const sql = `INSERT INTO tasks (task) VALUES ('${task}')`;
+    pool.query(sql, function(err){
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
+
+//Resgatando dados para editar
+app.get('/tasks/edit/:id', (req, res)=>{
+    const id = req.params.id
+    const sql = `SELECT*FROM tasks WHERE id = ${id}`
+    pool.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+        }
+        const tasks = data[0]
+        res.render('edit', {tasks})
+    })
+})
+
+//Alterar os dados
+app.post('/tasks/updatetask', (req, res)=>{
+    const id = req.body.id
+    const task = req.body.task
+    const sql = `UPDATE tasks SET task = '${task}' WHERE id = ${id}`
+    pool.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
+
+//Apagar os dados
+app.get('/tasks/remove/:id', (req, res)=>{
+    const id = req.params.id
+    const sql = `DELETE FROM tasks where id= ${id}`
+    pool.query(sql, function(err){
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
 
 
 
